ac89adb4e2d38b395aca9e97525d11b98cf4aa45
Yulia Shishman <yulia.shishman@gmail.com>
2019-02-26 19:08:30 +0200

    Initial Commit

---
+++ HW1/HW1_Selenium/TestMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace HW1_Selenium
+{
+ 
+    public class TestMethod
+    {
+         
+    }
+}
