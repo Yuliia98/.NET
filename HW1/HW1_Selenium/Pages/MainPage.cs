ad42cac19f5fc3773526bc9516a705e4e366767d
serhiiyasenev <serhii.yasenev@gmail.com>
2019-03-01 22:56:51 +0200

    upd

---
+++ HW1/HW1_Selenium/Pages/MainPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace HW1_Selenium.Pages
+{
+    public class MainPage : BasePage
+    {
+       public MainPage(IWebDriver driver) : base(driver) 
+       {
+       } 
+
+        [FindsBy(How = How.XPath, Using = "//ul[@class='header-top-menu']/li/a[contains(@href, '/promo/events/')]")]
+        public IWebElement PromoButton;
+    }
+}
